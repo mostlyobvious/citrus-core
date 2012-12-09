87709edef7b5a803d60332d2f5012794eaac6aa4
Paweł Pacana <pawel.pacana@gmail.com>
2012-12-09 15:05:41 +0100

    That didn't really felt like belonging to domain.

--- test/changeset_test.rb
+++ test/changeset_test.rb
@@ -6,8 +6,10 @@ module Citrus
   module Core
     class ChangesetTest < MiniTest::Unit::TestCase
 
-      def sample_github_payload
-        File.read(File.join(File.dirname(__FILE__), 'support/sample_payload.json'))
+      def sample_changeset
+        commit     = mock('commit')
+        repository = mock('repository')
+        Changeset.new(repository, [commit])
       end
 
       def test_should_be_initialized_with_repository_and_commits
@@ -17,24 +19,11 @@ module Citrus
       end
 
       def test_should_respond_to_commits
-        commit     = mock('commit')
-        repository = mock('repository')
-        changeset = Changeset.new(repository, [commit])
-        assert_respond_to changeset, :commits
+        assert_respond_to sample_changeset, :commits
       end
 
-      def test_should_create_instance_from_github_payload
-        assert_respond_to Changeset, :from_github_payload
-      end
-
-      def test_should_have_commits_from_github_payload
-        changeset = Changeset.from_github_payload(sample_github_payload)
-        assert_kind_of Commit, changeset.commits.first
-      end
-
-      def test_should_have_repository_from_github_payload
-        changeset = Changeset.from_github_payload(sample_github_payload)
-        assert_kind_of Repository, changeset.repository
+      def test_should_respond_to_repository
+        assert_respond_to sample_changeset, :repository
       end
 
     end
