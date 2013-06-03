2768c27a045beb99cd22a04927628671e6f0fdea
Paweł Pacana <pawel.pacana@gmail.com>
2013-06-03 10:12:31 +0200

    Update examples to support subscribers.

--- citrus_web.rb
+++ citrus_web.rb
@@ -1,15 +1,17 @@
 require 'sinatra'
 require 'citrus/core'
 
+helpers do
+  def build_succeeded(build)
+    puts "[#{build.uuid}] Sukces, milordzie!"
+  end
+end
+
 post '/' do
   adapter   = Citrus::Core::GithubAdapter.new
   changeset = adapter.create_changeset_from_push_data(params[:payload])
   build     = Citrus::Core::Build.new(changeset)
   service   = Citrus::Core::ExecuteBuildService.new
-
-  if service.start(build).result == 0
-    puts "[#{build.uuid}] Sukces, milordzie!"
-  else
-    puts "[#{build.uuid}] Poracha, milordzie."
-  end
+  service.add_subscriber(self)
+  service.start(build)
 end
