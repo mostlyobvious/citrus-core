fcd32d4e1d0a6f16468c92bdbc7562cd08270b45
Paweł Pacana <pawel.pacana@gmail.com>
2013-06-21 12:44:47 +0200

    Add code coverage badge and metrics.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,16 @@
-require 'bogus/rspec'
-require 'fakefs/spec_helpers'
 require 'citrus/core'
+require 'fakefs/spec_helpers'
 
 RSpec.configure do |config|
   config.order = 'random'
 end
 
+require 'bogus/rspec'
 Bogus.configure do |config|
   config.search_modules << Citrus::Core
 end
+
+require 'coveralls'
+Coveralls.wear!
+
+
