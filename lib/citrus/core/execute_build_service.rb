02a402784a8caf58de5b610afd12388e0325549c
Paweł Pacana <pawel.pacana@gmail.com>
2013-08-11 10:50:27 +0200

    Move publishing to private methods.
    They make visual clutter and I'd love to see them somewhere else
    so aspects maybe?

--- lib/citrus/core/execute_build_service.rb
+++ lib/citrus/core/execute_build_service.rb
@@ -16,13 +16,27 @@ module Citrus
       def start(build)
         path = workspace_builder.create_workspace(build)
         configuration = configuration_loader.load_from_path(path)
-        publish(:build_started, build)
+        notify_build_start(build)
         result = test_runner.start(configuration, path)
-        publish(:build_succeeded, build, result.output) if result.success?
-        publish(:build_failed, build, result.output)    if result.failure?
+        notify_build_result(build, result)
       rescue ConfigurationError => error
+        notify_build_abort(build, error)
+        raise
+      end
+
+      protected
+
+      def notify_build_start(build)
+        publish(:build_started, build)
+      end
+
+      def notify_build_result(build, result)
+        publish(:build_succeeded, build, result.output) if result.success?
+        publish(:build_failed,    build, result.output) if result.failure?
+      end
+
+      def notify_build_abort(build, error)
         publish(:build_aborted, build, error)
-        raise error
       end
 
     end
