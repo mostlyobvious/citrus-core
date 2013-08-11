5425bd6d5ebf6c99cf2fbc56db3fab9ce0ed4516
Paweł Pacana <pawel.pacana@gmail.com>
2013-08-11 10:33:32 +0200

    Build holds now output from build process.

--- lib/citrus/core/build.rb
+++ lib/citrus/core/build.rb
@@ -1,14 +1,17 @@
 require 'securerandom'
+require 'stringio'
 
 module Citrus
   module Core
     class Build
 
-      attr_reader :changeset, :uuid
+      attr_reader   :changeset, :uuid
+      attr_accessor :output
 
       def initialize(changeset, uuid = SecureRandom.uuid)
         @changeset = changeset
         @uuid      = uuid
+        @output    = StringIO.new
       end
 
     end
